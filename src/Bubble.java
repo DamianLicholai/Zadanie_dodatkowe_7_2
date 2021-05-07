9320840b19e5b39a47672eb309acc35280bdcb24
dlich <dlicholai@gmail.com>
2021-05-07 21:08:48 +0200

    solution

---
+++ src/Bubble.java
@@ -0,0 +1,20 @@
+public class Bubble {
+    void b_sort(int array[]) {
+        int temp;
+        int change = 1;
+        while (change > 0) {
+            change = 0;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                    change++;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
